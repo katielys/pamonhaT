70fe5a0a421c027ff521618493e76635d0bf4085
Katiely <katiely.eu@gmail.com>
2019-01-20 14:39:37 -0400

    [DEL]
    bug safado

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from telegram.ext import Updater, InlineQueryHandler, CommandHandler
 import requests
 import re
-
+token = 'AAGkohZwDu86zEwW4Wfj8j7JRxTAIg_oEl4'
 def get_url():
     contents = requests.get('https://random.dog/woof.json').json()
     url = contents['url']
@@ -12,10 +12,15 @@ def bop(bot, update):
     chat_id = update.message.chat_id
     bot.send_photo(chat_id=chat_id, photo=url)
 
+def sendTexto(bot, update):
+    chat_id = update.message.chat_id
+    bot.send_message(chat_id=chat_id,text="Dale o comando :/auau")
 def main():
     updater = Updater('722473469:AAGkohZwDu86zEwW4Wfj8j7JRxTAIg_oEl4')
     dp = updater.dispatcher
-    dp.add_handler(CommandHandler('start',bop))
+    dp.add_handler(CommandHandler('auau',bop))
+    dp.add_handler(CommandHandler('start',sendTexto))
+    ##updaterreply_text("Ta no inicio, calma ai ")
     updater.start_polling()
     updater.idle()
 
